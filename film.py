aca9a6aa1d0f11923c9ec2046d5d133795abc01c
Wallasfps <al21020041@itsa.edu.mx>
2023-12-28 20:45:34 -0600

    Actualizacion de codigo

--- film.py
+++ film.py
@@ -3,7 +3,7 @@ import psycopg2
 #?imagen del esquema de la base de datos
 #? https://www.postgresqltutorial.com/wp-content/uploads/2018/03/dvd-rental-sample-database-diagram.png
 '''
-	Walter Jahir Ambriz Reyna
+	Walter Jahir Ambriz Reyna 21020041
 	Ana Laura Cortez Cortes
 	Daniel Alejandro Gonzalez Rodriguez
 '''
